b672c074b0a32e53ca4ad7e9aae91698f08e935c
Tom van Pelt <tomtcvanpelt@gmail.com>
2021-12-05 15:11:57 +0100

    cleaned up code, added correct articles

--- barQRGui/barQRGui.py
+++ barQRGui/barQRGui.py
@@ -1,38 +1,52 @@
-from guizero import App, Text, Window, PushButton, Picture, Box, TextBox
+from guizero import App, Text, Window, Picture, Box
 from pynput.keyboard import Listener, Key
-import threading
-
-workshop = "Houtwerkplaats"
 
 # ===============================================
 
-app = App(title="IncheckProto", width=800, height=480)
-#app.set_full_screen()
+workshopList = ["Aqualab", "Elektrolab", "Energielab", "Houtwerkplaats", "Lijmlab", "Materialenlab", "Metaalwerkplaats", "Mobiliteitslab", "Protolab"]
+articleList = ["de", "het"]
 
-text = Text(app, "\n")
-pictureBox = Box(app)
-hrlogo = Picture(pictureBox, image="hrlogo3.png", width=750, height=150)
+# ===============================================
 
-text1 = Text(app, "\nWelkom bij de {}".format(workshop), size=20)
-text2 = Text(app, "Scan uw QR-code in de HR-app", size=20)
-text3 = Text(app, "Of scan de barcode op uw HR-pas", size=20)
+workshop = workshopList [8]
 
-windowSuccess = Window(app, title="Success", visible=False, width=800, height=480)
-#windowSuccess.set_full_screen()
-text = Text(windowSuccess, "\n")
-successImg = Picture(windowSuccess, image="vinkje.png", width=400, height=400)
+# ===============================================
 
-windowFailed = Window(app, title="Failed", visible=False, width=800, height=480)
-#windowFailed.set_full_screen()
-text = Text(windowFailed, "\n")
-failedImg = Picture(windowFailed, image="kruis.png", width=400, height=400)
+articleGUIText = ""
+if (workshop == workshopList [3] or workshop == workshopList [6]):
+    articleGUIText = articleList [0]
+else: 
+    articleGUIText = articleList [1] 
 
-def open_window(currentWindow):
-    currentWindow.show(wait=True)
-    currentWindow.after(3000, lambda: close_window(currentWindow))
+# ===============================================
 
-def close_window(currentWindow):
-    currentWindow.hide()
+app = App (title="RegistratiePaalGUI", width=800, height=480)
+app.set_full_screen ()
+
+text = Text (app, "\n")
+pictureBox = Box (app)
+hrlogo = Picture (pictureBox, image="hrlogo3.png", width=750, height=150)
+
+text1 = Text (app, "\nWelkom bij {} {}".format (articleGUIText, workshop), size=20)
+text2 = Text (app, "Scan uw QR-code in de HR-app", size=20)
+text3 = Text (app, "Of scan de barcode op uw HR-pas", size=20)
+
+windowSuccess = Window (app, title="Success", visible=False, width=800, height=480)
+windowSuccess.set_full_screen ()
+text = Text (windowSuccess, "\n")
+successImg = Picture (windowSuccess, image="vinkje.png", width=400, height=400)
+
+windowFailed = Window (app, title="Failed", visible=False, width=800, height=480)
+windowFailed.set_full_screen ()
+text = Text (windowFailed, "\n")
+failedImg = Picture (windowFailed, image="kruis.png", width=400, height=400)
+
+def open_window (currentWindow):
+    currentWindow.show (wait=True)
+    currentWindow.after (3000, lambda: close_window (currentWindow))
+
+def close_window (currentWindow):
+    currentWindow.hide ()
 
 # ===============================================
 
@@ -41,32 +55,35 @@ listener = None
 
 def listToString (inputList): 
     returnString = "" 
-    return (returnString.join(inputList))
+    return (returnString.join (inputList))
 
 def on_press (key):
     if (key == Key.enter):
-        print (studentNumList)
-        codeType = studentNumList.pop (0)
-
-        if (codeType == 'b' or codeType == 'B'): 
-            print ("CodeType: Barcode (Code 39)")
-            codeType = "Barcode"
-        elif (codeType == 'Q' or codeType == 'q'):
-            print ("CodeType: QR-Code")
-            codeType = "QR-code"
-
-        studentNumString = listToString (studentNumList)
-        print (studentNumString)
-        studentNumList.clear ()
-
-        if (studentNumString.isnumeric() and len (studentNumString) == 7):
-            #success
-            print ("Success")
-            open_window (windowSuccess)
-        else:
-            #failed
-            print ("Failed")
+        #print (studentNumList)
+        if not (studentNumList):
             open_window (windowFailed)
+        else:   
+            codeType = studentNumList.pop (0)
+
+            if (codeType == 'b' or codeType == 'B'): 
+                print ("CodeType: Barcode (Code 39)")
+                codeType = "Barcode"
+            elif (codeType == 'Q' or codeType == 'q'):
+                print ("CodeType: QR-Code")
+                codeType = "QR-code"
+
+            studentNumString = listToString (studentNumList)
+            print (studentNumString)
+            studentNumList.clear ()
+
+            if (studentNumString.isnumeric () and len (studentNumString) == 7):
+                #success
+                print ("Success")
+                open_window (windowSuccess)
+            else:
+                #failed
+                print ("Failed")
+                open_window (windowFailed)
     
     elif (hasattr (key, 'char')):
         studentNumList.append (key.char)
